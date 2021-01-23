23e8c3c7085177e998e8dddb9266c9c1b8fc2766
tika27 <77032551+tika27@users.noreply.github.com>
2021-01-22 21:34:10 -0700

    added prompt

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,23 @@ var generateBtnEl = document.querySelector("#generate");
 var btncopyEl = document.querySelector("#copy");
 
 generateBtnEl.addEventListener("click", function () {
-var charlength = prompt ("how many words would you perfer for your new password? Please pick a # between 8 to 20.");
-if ((charlength >=8) && (charlength <= 20)){
+var charlength = prompt ("how many words would you perfer for your new password? Please pick a # between 8 to 128.");
 
-  let symbols = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~;
+if ((charlength >=8) && (charlength <= 128)){
+
+  let symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   let numbers = "0123456789";
   let lowerCase = "abcdefghijklmonpqrstuvwxy";
   let upperCase = "ABCDEFGHIJCLMOPQRSTUVQXYZ";
   
   let newPassword = "";
+
+  //prompt variables
+  let useLowerCase = confirm("Do you want to use lowercase characters?");
+  let useUpperCase = confirm("Do you want to use uppercase letters?");
+  let useNumeric = confirm("Do you eant to use numbers?");
+  let useSpecial = confirm("Do you want to use special charaters?");
+
 }
 }
 
@@ -24,17 +32,17 @@ if ((charlength >=8) && (charlength <= 20)){
 
 
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
