feef471cadbb83df5322ce721c05720aacf1dd37
tika27 <77032551+tika27@users.noreply.github.com>
2021-01-22 20:50:31 -0700

    declare variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,26 @@
+//declare variables 
+var passwordEl = document.querySelector("#password");
+var generateBtnEl = document.querySelector("#generate");
+var btncopyEl = document.querySelector("#copy");
+
+generateBtnEl.addEventListener("click", function () {
+var charlength = prompt ("how many words would you perfer for your new password? Please pick a # between 8 to 20.");
+if ((charlength >=8) && (charlength <= 20)){
+
+  let symbols = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~;
+  let numbers = "0,1,2,3,4,5,6,7,8,9";
+  let lowerCase = "a,b,c,d,"
+}
+}
+
+
+
+)
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
