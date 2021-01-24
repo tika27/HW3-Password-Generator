74a0ce1b01261d4d08ae2cb349bbb9843d075c72
tika27 <77032551+tika27@users.noreply.github.com>
2021-01-23 21:28:13 -0700

    modified

--- Develop/script.js
+++ Develop/script.js
@@ -149,3 +149,10 @@ btngenerateEl.addEventListener("click", function () {
     }
 
 })
+
+
+
+//This assignment was extremly hard. 
+// I had no idea what i was suppose to do however, 
+// with help of internet and my tutor and online assistant I have been able to do this much. 
+// It took me around 32hrs of non-stop cross checking between console.log and 20+ hrs of youtube.
